9b931d6e7852340ca991390b9a63030ace34dfdf
Abhishek Yadav <abhiydv222@gmail.com>
2025-11-11 15:37:10 +0530

    contact page design

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
     <div class="auto-container">
         <div class="title-outer">
             <ul class="page-breadcrumb">
-                <li><a href="index.html">Home</a></li>
+                <li><a href="index.php">Home</a></li>
                 <li>Contact Us</li>
             </ul>
             <h1 class="title">Contact Us</h1>
@@ -20,7 +20,40 @@
             <div class="row g-5">
                 <div class="col-lg-6 content-column">
                     <div class="inner-column">
-                        
+                        <div class="card  mb-3">
+                            <div class="card-body d-flex align-items-start connt_font">
+                              <i class="fa-light fa-envelope color-white font_icon d-inline-block rounded-circle"></i>
+                              <div class="ps-4">
+                                <h3 class="h4 pb-1 mb-2">Email us</h3>
+                                <p>Please feel free to drop us a line. We will respond as soon as possible.</p>
+                                <a href="mailto: info@cloudwisekpo.com" class="btn btn-contact px-0">
+                                   info@cloudwisekpo.com
+                                  <i class="bx bx-right-arrow-alt fs-4 ms-1"></i>
+                                </a>
+                              </div>
+                            </div>
+                            <hr>
+                            <div class="card-body d-flex align-items-start connt_font">
+                              <i class="fa-light fa-phone-flip color-white font_icon d-inline-block rounded-circle"></i>
+                              <div class="ps-4">
+                                <h3 class="h4 pb-1 mb-2">Call any time</h3>
+                                <p>If you need immediate assistance feel free to call us any time.</p>
+                                <a href="tel:+919999999999" class="btn btn-contact px-0">+91 9999999999</a>
+                              </div>
+                            </div>
+                            <hr>
+                            <div class="card-body d-flex align-items-start connt_font">
+                                <i class="fa-light fa-location-dot color-white font_icon d-inline-block rounded-circle"></i>
+                              <div class="ps-4">
+                                <h3 class="h4 pb-1 mb-2">Visit us</h3>
+                                <p> Chhikara Colony Gali No 6 Bahadurgarh, Haryana 124507</p>
+                                <a href="https://maps.app.goo.gl/jTooPPzYb68Uph5g7" target="_blank" class="btn btn-contact px-0">
+                                  Get directions
+                                  <i class="bx bx-right-arrow-alt fs-4 ms-1"></i>
+                                </a>
+                              </div>
+                            </div>
+                          </div>
                     </div>
                 </div>
                 <div class="col-lg-6 content-column">
@@ -30,19 +63,18 @@
                             <h3 class="title wow splt-txt mt-3" data-splitting>Get in touch for expert service</h3>
                             <p class="text wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">We'd love to hear from you! If you have any questions.</p>
                         </div>
-                        <form id="contact_form" name="contact_form" action="includes/sendmail.php" method="post">
+                        <form id="contact_form" name="contact_form" action="#" method="post">
                             <div class="type-feild">
                                 <i class="fa-light fa-user"></i>
-                                <input name="form_name" class="required" type="text" placeholder="Your Name*">
+                                <input name="name" class="required" type="text" placeholder="Your Name*">
                             </div>
                             <div class="type-feild">
                                 <i class="fa-light fa-envelope"></i>
-                                <input name="form_email" class="required" type="email" placeholder="Email Address*">
+                                <input name="email" class="required" type="email" placeholder="Email Address*">
                             </div>
                             <div class="type-feild">
                                 <i class="fa-light fa-pen-to-square"></i>
-                                <textarea name="form_message" class="required"
-                                    placeholder="Enter Your Messege here"></textarea>
+                                <textarea name="message" class="required" placeholder="Enter Your Messege here"></textarea>
                             </div>
                             <button class="btn-two"><i class="fa-light fa-paper-plane"></i> Get In Touch</button>
                         </form>
@@ -54,6 +86,6 @@
 </section>
 <!-- Page Contact area end here -->
 
-
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3499.536645335759!2d76.92592567486307!3d28.70350417562612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d095b35bfecbb%3A0x5e2de6ef23b772db!2sGali%20Number%206%2C%20Bahadurgarh%2C%20Haryana%20124507!5e0!3m2!1sen!2sin!4v1762854226779!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 <?php include('footer.php');?>
